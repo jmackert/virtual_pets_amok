eb0533428341bbc79dcdd5b625a3d2da901db726
jmackert <jonahbmackert@gmail.com>
2019-09-11 21:50:38 -0400

    added interaction menu

--- src/main/java/virtual_pets_amok/VirtualPetsAmokApp.java
+++ src/main/java/virtual_pets_amok/VirtualPetsAmokApp.java
@@ -1,17 +1,29 @@
 package virtual_pets_amok;
 
+import java.util.Scanner;
+
 public class VirtualPetsAmokApp {
+	static Scanner input = new Scanner(System.in);
+	static boolean running = true;
+	static Shelter myShelter = new Shelter();
+
+	public void interactionMenu() {
+		System.out.println("1: Feed all the organic animals");
+		System.out.println("2: Give water to all the organic animals");
+		System.out.println("3: Clean the litter box");
+		System.out.println("4: Walk the dogs");
+	}
 
 	public static void main(String[] args) {
-		
-		boolean running = true;
-		Shelter myShelter = new Shelter();
-		
+
 		while (running == true) {
-			myShelter.addOrganicDog();
-			myShelter.addOrganicCat();
-			//myShelter.addRoboticDog();
-			//myShelter.addRoboticCat();
+			System.out.println("1 for add organic dog .... 2 for add organic cat: ");
+			int add = input.nextInt();
+			if (add == 1) {
+				myShelter.addOrganicDog();
+			} else {
+				myShelter.addOrganicCat();
+			}
 			myShelter.showShelter();
 
 		}
